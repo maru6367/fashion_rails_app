b51100e4efaaea1be7ba1faa4ec148d8c69c785f
丸山堅也 <maruyamakenya@maruyamakennarinoMacBook-Air.local>
2025-12-03 03:37:31 -0800

    ProductsControllerとルーティングを追加

---
+++ db/migrate/20251202132321_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :category
+      t.integer :price
+      t.text :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
